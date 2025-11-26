1415cc707e0c6e08c6e4fd58572ad130bbee8700
UnhappyAlex <alexandr.bro03@gmail.com>
2025-11-27 02:07:06 +0500

    Added functionality for working with identifiers

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,22 +1,35 @@
+import { useLoaderData, Link, useSubmit } from "react-router-dom";
 
+export default function TodoList() {
+    const list = useLoaderData();
+    const submit = useSubmit();
+    
+    const handleDoneClick = key => {
+        submit(null, {action: `/${key}`, method: 'patch'});
+    };
 
-export default function TodoList(props) {
+    const handleDeleteClick = key => {
+        submit(null, {action: `/${key}`, method: 'delete'});
+    };
+
     return (
         <section>
             <h1>Дела</h1>
             <table className="table is-hoverable is-fullwidth">
                 <tbody>
-                    {props.list.map(item => (
+                    {list.map(item => (
                         <tr key={item.key}>
                             <td>
-                                {item.done && <del>{item.title}</del>}
-                                {!item.done && item.title}
+                                <Link to={`/${item.key}`}>
+                                    {item.done && <del>{item.title}</del>}
+                                    {!item.done && item.title}
+                                </Link>
                             </td>
                             <td>
                                 <button className="button is-success"
                                         title="Выполнено"
-                                        // disabled={item.done}
-                                        onClick={() => {props.setDone(item.key)}}
+                                        disabled={item.done}
+                                        onClick={() => {handleDoneClick(item.key)}}
                                 >
                                     &#9745;
                                 </button>
@@ -24,7 +37,7 @@ export default function TodoList(props) {
                             <td>
                                 <button className="button is-danger"
                                         title="Удалить"
-                                        onClick={() => props.del(item.key)}
+                                        onClick={() => {handleDeleteClick(item.key)}}
                                 >
                                     &#9746;
                                 </button>
