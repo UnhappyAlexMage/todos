e95d405b4040b71a8656bca9bc6cf2e2292ece3a
UnhappyAlex <alexandr.bro03@gmail.com>
2025-11-27 02:03:24 +0500

    Routes added

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,17 @@ import { createBrowserRouter, createRoutesFromElements, Route } from "react-rout
 import App from "./App.js";
 import TodoList from "./TodoList.js";
 import TodoAdd from "./TodoAdd.js";
-
-import todos from "./todos.js";
+import getTodos, { addTodo, getTodo, actTodo } from "./api.js";
+import TodoDetails from "./TodoDetail.js";
+import Error404 from "./Error404.js";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<App />}>
-            <Route index={true} element={<TodoList list={todos}/>} />
-            <Route path="add" element={<TodoAdd />}/>
+            <Route index={true} element={<TodoList />} loader={ getTodos } />
+            <Route path="add" element={<TodoAdd />} action={ addTodo } />
+            <Route path=':key' element={<TodoDetails />} loader={ getTodo } action={ actTodo } errorElement={<Error404 />} />
+            {/* <Route path="*" element={<ErrorPageNotFound/>}/> */}
         </Route>
     )
 );
